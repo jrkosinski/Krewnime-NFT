709a84466fd73bece0fc33b29640c6812a06e886
Acer <john.kosinski@gmail.com>
2022-08-11 16:26:13 +0700

    added lowgas version

---
+++ EVM/krewnime-nft-lowgas/contracts/KrewnimeNFT.sol
@@ -0,0 +1,225 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./ERC721A/ERC721A.sol"; 
+
+//TODO: comment it up 
+//TODO: order the functions 
+//TODO: remove change to ERC721, and remove test 
+//TODO: account for initialOwner in ctor, and add test 
+//TODO: replace to.be.reverted with to.be.revertedWith
+//TODO: pack the values 
+
+/**
+ * @title The Krewnime NFT Collection (low gas usage)
+ * @author John R. Kosinski 
+ * 
+ * This project allows the single contract owner to mint all tokens at once or individually, 
+ * with the option to (a) add more tokens to the collection in the future and mint them, 
+ * and (b) to create a new version of the store contract if desired, in order to change the 
+ * rules for the selling and minting of NFTs. 
+ * 
+ * The design creates a basic NFT contract that allows for: 
+ * - changing the collection size (adding to the collection) 
+ * - receiving royalties (ERC-2981) 
+ * - enumerable 
+ * - mintable 
+ * - burnable 
+ * - URI storage 
+ * - ADMIN and MINTER security roles 
+ * 
+ * The business rules for selling and minting are stored separately in the TokenMintStore 
+ * contract. If the business rules change, that contract can be decommissioned and replaced
+ * by another contract, which is assigned the Mintable role for the NFT, replacing the 
+ * old store with the new one. 
+ * 
+ * The base use case here is for the collection owner to mint the entire collection 
+ * initially, to be sold on a marketplace. 
+ * 
+ * Remedial action upon compromise is to pause the contract. 
+ */
+contract KrewnimeNFT is ERC721A {
+    address public tokenAdminAddress;   //TODO: pack all values 
+    address public tokenMinterAddress;  
+    
+    uint256 public maxSupply = 10; 
+    uint256 public collectionSize = 10; 
+    
+    error NotAuthorized();
+    error CollectionSizeExceeded();
+    error MaxSupplyExceeded(); 
+    error InitialMintAlreadyDone();
+    
+    mapping(uint256 => uint256) private tokenIdsToUris; 
+    
+    modifier adminOnly {
+        if (!hasRole(keccak256("ADMIN"), _msgSenderERC721A())) {
+            revert NotAuthorized();
+        }
+        _; 
+    }
+    
+    modifier minterOnly {
+        if (!hasRole(keccak256("MINTER"), _msgSenderERC721A())) {
+            revert NotAuthorized();
+        }
+        _; 
+    }
+    
+    /**
+     * @dev Constructor. 
+     * 
+     * @param initialOwner Initial owner address (if 0x0, owner becomes msg.sender)
+     * @param tokenName NFT token name 
+     * @param tokenSymbol NFT token symbol 
+     * @param _maxSupply Number of items allowed to be minted
+     * @param _collectionSize Number of items in the collection 
+     */
+    constructor(
+        address initialOwner,
+        string memory tokenName, 
+        string memory tokenSymbol,
+        uint256 _maxSupply, 
+        uint256 _collectionSize
+    ) ERC721A(tokenName, tokenSymbol) {
+        if (_collectionSize > _maxSupply) 
+            revert MaxSupplyExceeded(); 
+        
+        tokenAdminAddress = (initialOwner == address(0) ? msg.sender : initialOwner); 
+        maxSupply = _maxSupply;
+        collectionSize = _collectionSize;
+    }
+    
+    /**
+     * @dev Allows authorized caller (minter role only) to mint one. 
+     * 
+     * @param to The address of the token recipient once minted. 
+     * @return The token ID of the minted token. 
+     */
+    function mintNext(address to) external minterOnly returns (uint256) {
+        return _mintNext(to); 
+    }
+    
+    /**
+     * @dev Mints the specified number of items in the collection to the given address. 
+     * 
+     * If the address contains part of the collection already, starts from the last one 
+     * minted to that address. 
+     * The last index of the collection won't be exceeded. 
+     * @return The number of tokens minted. 
+     */
+    function multiMint(address to, uint256 count) external minterOnly returns(uint256) {
+        if (count > collectionSize) 
+            revert CollectionSizeExceeded(); 
+        
+        //get the start index & limit
+        uint256 startIndex = this.balanceOf(to); 
+        uint256 limit = startIndex + count; 
+        if (limit > collectionSize) {
+            limit = collectionSize;
+        }
+        
+        //mint tokens and count number minted
+        uint256 numberMinted = 0;
+        for(uint n=startIndex; n<limit; n++) {
+            _mintNext(to);
+            numberMinted++;
+        }
+        
+        return numberMinted;
+    }
+    
+    /**
+     * @dev Mints the entire collection to the admin or owner (caller). 
+     * Will revert if totalSupply() > 0. 
+     */
+    function initialMint() external adminOnly {
+        if (totalSupply() > 0) 
+            revert InitialMintAlreadyDone();
+            
+        for(uint n=0; n<collectionSize; n++) {
+            _mintNext(_msgSenderERC721A());
+        }
+    }
+    
+    /**
+     * @dev Owner of a token may burn or destroy it. 
+     * 
+     * @param tokenId The id of the token to burn. 
+     */
+    function burn(uint256 tokenId) external {
+        return _burn(tokenId, true);
+    }
+    
+    function hasRole(bytes32 role, address account) public view returns (bool) {
+        if (role == keccak256("ADMIN")) {
+            return (account == tokenAdminAddress);
+        }
+        else if (role == keccak256("MINTER")) {
+            return (account == tokenMinterAddress) || (account == tokenAdminAddress);
+        }
+        
+        return false;
+    }
+    
+    function transferRole(bytes32 role, address account) external {
+        //TODO: implement
+    }
+    
+    function setMinter(address account) external adminOnly {
+        tokenMinterAddress = account;
+    }
+    
+    /**
+     * @dev Returns the URI of the specified token. 
+     * 
+     * @param tokenId The id of a token whose URI to return.
+     * @return string Token URI. 
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        string memory uri = string(abi.encodePacked(_baseURI(), _toString(tokenIdsToUris[tokenId])));        
+        return string(abi.encodePacked(uri, _uriSuffix())); 
+    }
+    
+    /**
+     * @dev Admin can change the maxSupply - the max number of items that can be minted, 
+     * and the collections size - the max that can be minted per owner. 
+     * 
+     * @param _maxSupply The new value to set for maxSupply. 
+     * @param _collectionSize The new value to set for collectionSize. 
+     */
+    function setSupplyParameters(uint256 _maxSupply, uint256 _collectionSize) external adminOnly {
+       if (_collectionSize > _maxSupply)
+            revert MaxSupplyExceeded(); 
+            
+        maxSupply = _maxSupply;
+        collectionSize = _collectionSize;
+    }
+    
+    
+    function _mintNext(address to) private returns (uint256) {
+        if (this.totalSupply() >= maxSupply)
+            revert MaxSupplyExceeded();
+        if (this.balanceOf(to) >= collectionSize)
+            revert CollectionSizeExceeded();
+            
+        uint256 tokenId = _nextTokenId();
+        _safeMint(to, 1);
+        tokenIdsToUris[tokenId] = this.balanceOf(to);
+        return tokenId;
+    }
+    
+    function _startTokenId() internal pure override returns (uint256) {
+        return 1;
+    }
+    
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://base_uri/"; //TODO: update this to real value 
+    }
+    
+    function _uriSuffix() internal view virtual returns (string memory) {
+        return ".json"; 
+    }
+}
---
+++ EVM/krewnime-nft-lowgas/contracts/TokenMintStore.sol
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./IMintable.sol";
+
+/**
+ * @title The Krewnime NFT Collection (low gas usage)
+ * @author John R. Kosinski 
+ * 
+ * This project allows the single contract owner to mint all tokens at once or individually, 
+ * with the option to (a) add more tokens to the collection in the future and mint them, 
+ * and (b) to create a new version of the store contract if desired, in order to change the 
+ * rules for the selling and minting of NFTs. 
+ * 
+ * The design creates a basic NFT contract that allows for: 
+ * - changing the collection size (adding to the collection) 
+ * - receiving royalties (ERC-2981) 
+ * - enumerable 
+ * - mintable 
+ * - burnable 
+ * - URI storage 
+ * - ADMIN and MINTER security roles 
+ * 
+ * The business rules for selling and minting are stored separately in the TokenMintStore 
+ * contract. If the business rules change, that contract can be decommissioned and replaced
+ * by another contract, which is assigned the Mintable role for the NFT, replacing the 
+ * old store with the new one. 
+ * 
+ * This contract can work with any IMintable. 
+ * 
+ * To enable it, create it with the address  of the NFT contract to be minted in 
+ * the constructor. Then, for the NFT contract (which should implement role-based  
+ * security) enable the MINTER role for this TokenMintStore contract. Now this contract 
+ * is set up to sell mints of the specified NFT. 
+ * 
+ * This contract is a simple Ownable. The remedial action upon compromise is to either 
+ * pause this contract, remove its MINTER role from the associated NFT contract, or both.
+ */
+contract TokenMintStore {
+    IMintable public nftContract; //TODO: pack all values 
+    uint256 public mintPrice = 0; 
+    address owner = address(0); 
+    
+    /**
+     * @dev Emitted when an attempt is made to withdraw ether from contract.
+     */
+    event Withdraw (
+        address sender,
+        uint256 amount,
+        bool success
+    );
+    
+    /**
+     * @dev Emitted when a request to mint completes without reverting. 
+     */
+    event Mint (
+        address sender, 
+        address to, 
+        uint256 amount,
+        uint256 number
+    );
+    
+    /**
+     * @dev Constructor. 
+     * 
+     * @param _nftContract The address of the NFT contract whose items are being sold. 
+     * @param _mintPrice The price charged per unit to mint. 
+     */
+    constructor(IMintable _nftContract, uint256 _mintPrice) {
+        nftContract = _nftContract;
+        mintPrice = _mintPrice;
+        owner = msg.sender;
+    }
+    
+    /**
+     * @dev Owner can set the price to mint an NFT. 
+     * 
+     * @param _mintPrice The price to set. 
+     */
+    function setMintPrice(uint256 _mintPrice) external  {
+        require(msg.sender == owner, "Permission denied");
+        mintPrice = _mintPrice;
+    }
+    
+    /**
+     * @dev Anyone can purchase the right to mint, and mint an NFT from contract. 
+     * 
+     * @param to The NFT token recipient. 
+     * @return tokenId The ID of the minted token. 
+     */
+    function mintNext(address to) external payable returns(uint256) {
+        require(address(nftContract) != address(0), "TokenMintStore: NFT address not set");
+        require(msg.value >= getMintPrice(msg.sender), "TokenMintStore: transferred value less than price");
+        
+        uint256 id = nftContract.mintNext(to); 
+        
+        emit Mint(msg.sender, to, msg.value, 1); 
+        
+        return id; 
+    }
+    
+    /**
+     * @dev Sell multiple mints in the collection to a single user. Will be reverted if the 
+     * number to be minted exceeds the number allowed.
+     * 
+     * @param to The NFT token recipient. 
+     * @return The number minted and sold. 
+     */
+    function multiMint(address to, uint256 count) external payable returns (uint256) {
+        require(address(nftContract) != address(0), "TokenMintStore: NFT address not set");
+        
+        uint256 numberMinted = nftContract.multiMint(to, count); 
+        require(msg.value >= (numberMinted * getMintPrice(msg.sender)), "TokenMintStore: transferred value less than price");
+        
+        emit Mint(msg.sender, to, msg.value, numberMinted); 
+        
+        return numberMinted;
+    }
+    
+    /**
+     * @dev Contract owner can withdraw all collected funds from contract. 
+     * 
+     * @return True if successful. 
+     */
+    function withdrawAll() external returns (bool) {
+        require(msg.sender == owner, "Permission denied");
+        
+        uint256 amount = address(this).balance;
+        (bool success,) = owner.call{value:amount}(""); 
+        
+        emit Withdraw(owner, amount, success);
+        return success;
+    }
+    
+    /**
+     * @dev Returns either a custom or standard price as appropriate for the given 
+     * recipient. 
+     * 
+     * @return price The appropriate price for the recipient. 
+     */
+    function getMintPrice(address /*buyer*/) internal view returns (uint256 price) {
+        return mintPrice;
+    }
+}
